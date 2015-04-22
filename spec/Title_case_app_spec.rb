44c7bb0711b57a54b4b8bd104e60a0d073f9f45b
kha le & cory spitzer <student@epicodus.com>
2015-04-22 14:20:59 -0700

    Test a string for title case.

--- spec/Title_case_app_spec.rb
+++ spec/Title_case_app_spec.rb
@@ -1,2 +1,14 @@
 require('capybara/rspec')
 require('./app/app')
+require('sinatra')
+
+Capybara.app = Sinatra::Application
+
+describe('the title case path', {:type => :feature}) do
+  it('processes the user input and returns it title cased') do
+    visit('/')
+    fill_in('string', {:with => 'green eggs and ham'})
+    click_button('Submit')
+    expect(page).to(have_content('Green Eggs and Ham'))
+  end
+end
