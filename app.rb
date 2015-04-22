98f5e0c85edf349b884957fa4da3f81c3e7cba46
kha le & cory spitzer <student@epicodus.com>
2015-04-22 16:06:07 -0700

    Improve logic to capitalize first word and ignore preposition and conjunction. pass test.

--- app.rb
+++ app.rb
@@ -4,11 +4,17 @@ require('pry')
 
 class String
   define_method(:title_case) do
+    black_list = ["a", "and", "to", "the", "for", "in"]
     words = self.split(' ')
+    first_word = words.shift
     words.each do |word|
-      word.capitalize!
+      if black_list.include?(word.downcase())
+        word.downcase()
+      else
+        word.capitalize!
+      end
     end
-    words.join(' ')
+    first_word.capitalize!() + " " + words.join(' ')
   end
 end
 
